f6602ba39836c758acda7547d11fbde58ca20698
tonglecode <tongle.coding@gamil.com>
2025-01-17 11:24:06 +0900

    feat: 인증방식 변경

--- src/lib/graphql-client.ts
+++ src/lib/graphql-client.ts
@@ -1,7 +1,33 @@
 import { GraphQLClient } from "graphql-request";
+import { isServer } from "solid-js/web";
 
 const endpoint = import.meta.env.VITE_GRAPHQL_URL;
 
 export const gqlClient = new GraphQLClient(endpoint, {
-  credentials: "include",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  fetch: (input: RequestInfo | URL, init?: RequestInit) => {
+    if (!init) return fetch(input);
+
+    const { credentials, ...restOptions } = init;
+
+    // 서버 사이드에서는 쿠키 처리를 건너뜁니다
+    if (isServer) {
+      return fetch(input, restOptions);
+    }
+
+    // 클라이언트 사이드에서만 쿠키를 처리합니다
+    return fetch(input, {
+      ...restOptions,
+      headers: {
+        ...restOptions.headers,
+        ...(document.cookie.includes("qid") && {
+          Cookie: document.cookie
+            .split("; ")
+            .find((row) => row.startsWith("qid=")),
+        }),
+      },
+    });
+  },
 });
--- vite.congfig.ts
+++ vite.congfig.ts
@@ -4,11 +4,20 @@ import solid from "vite-plugin-solid";
 export default defineConfig({
   plugins: [solid()],
   server: {
-    port: 3000,
+    port: 3000, // 개발 서버 포트
   },
   define: {
     "process.env.VITE_GRAPHQL_URL": JSON.stringify(
       process.env.VITE_GRAPHQL_URL
     ),
+  },
+  build: {
+    outDir: "dist", // 빌드 결과가 저장될 경로
+    target: "esnext", // Cloudflare와 호환성을 위해 최신 JavaScript 사용
+    rollupOptions: {
+      output: {
+        entryFileNames: "index.js", // 진입 파일 이름
+      },
+    },
   },
 });
